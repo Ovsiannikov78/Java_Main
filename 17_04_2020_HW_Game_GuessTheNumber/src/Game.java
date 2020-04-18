b84ed01f03d89c058e053412af4546cde2f0703e
Ovsiannikov78 <seri_tanya@mail.ru>
2020-04-18 19:07:29 +0200

    deleted 17_04_2020_HW_Game_GuessTheNumber/src/GameTest

--- 17_04_2020_HW_Game_GuessTheNumber/src/Game.java
+++ 17_04_2020_HW_Game_GuessTheNumber/src/Game.java
@@ -6,30 +6,29 @@ public class Game {
     public static void main(String[] args) {
         guessTheNumber();
     }
-    /*  Task --------- Game-GuessTheNumber
-        The user must guess a number in the range 0 and 10.
-        The user has 3 attempts. */
+
+    /* The user must guess the number in the range from 0 to 10.
+       User has 3 attempts*/
 
     public static void guessTheNumber() {
         System.out.println("Let's start our game. You have 3 attempts.");
         System.out.println("You can enter numbers from 0 to 10.");
-        int number = (int) (Math.random() * 10);
-        for (int i = 3; i >= 1; i--) {
+        int number = (int)(Math.random() * 10);
+        for(int i = 3; i >= 1; --i) {
             System.out.println("-------- Please, enter your number ----------");
             int inputNumber = scanner.nextInt();
             if (inputNumber < 0 || inputNumber > 10) {
-                /* Используем i++ при условии,если мы хотим,что-бы введённое поильзователем не правильное число
-                 не учитывалось как попытка. И используем break , что-бы при введённом пользователем не правильном
-                  числе, осуществлялся выход из программы. И можно начать заново */
-                i++;
-                // break
+               /* использую i++ , чтобы не учитывалась попытка, при введённом неправильном числе от пользователя.
+                  И используем break, чтобы выйти из программы и начать с начала если введено неправильное число.   */
+                ++i;
+                //break;
                 System.out.println(" Wrong number ! You can enter numbers ONLY from 0 to 10 !!! ");
-            } else if (inputNumber == number) {
+            }else if (inputNumber == number) {
                 System.out.println(" You are winner :-) !!!");
                 break;
-            } else if (i != 1) {
+            }else if (i != 1) {
                 System.out.println("You didn't guess. You have left " + (i - 1) + " attempt.");
-            } else {
+            }else{
                 System.out.println("Game over :-( You can try again.");
                 break;
             }
@@ -37,9 +36,4 @@ public class Game {
         scanner.close();
     }
 }
-
-
-
-
-
 
